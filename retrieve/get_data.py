b7beb35aa98ba5d43124def00c609afb164c1a72
Justin Iverson <justiniver@icloud.com>
2026-02-05 19:39:43 -0500

    get_data and excel sheet

---
+++ retrieve/get_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+EXCEL_PATH = r"./excel_data/YieldData.xlsx"
+
+def get_yield(term: int):
+    term_s = f'{term} year'
+    cols = ['UK', 'CAN', 'DBR', 'FRN', 'SWISS', 'SEK', 'NOR', 'AUD', 'ITL', 'US', 'CNY']
+    df = pd.read_excel(EXCEL_PATH, sheet_name=term_s, index_col=0)
+    df.index = pd.to_datetime(df.index, format="%Y-%m-%d %H:%M:%S", errors="coerce")
+    df = df[df.index.notna()]
+
+    df.columns = cols
+    df = df.truncate(before = pd.to_datetime('2016-2-18'))
+    return df
+
+df_1 = get_yield(1)
+
+print(df_1.head())
