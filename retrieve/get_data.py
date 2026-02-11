6e43780526353a2ea8deed818d74d881c4b0a544
Justin Iverson <justiniver@icloud.com>
2026-02-10 19:59:56 -0500

    documentation and cleaning up files

--- retrieve/get_data.py
+++ retrieve/get_data.py
@@ -10,5 +10,6 @@ def get_yield(term: int):
     df = df[df.index.notna()]
 
     df.columns = cols
+    # This is necessary because some sheets are missing first few rows of data
     df = df.truncate(before = pd.to_datetime('2016-2-18'))
     return df
