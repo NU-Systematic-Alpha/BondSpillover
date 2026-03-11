b711458fbd678d0012f3ffb1de7b8bf52fbebb7f
Justin Iverson <justiniver@icloud.com>
2026-03-10 21:58:57 -0400

    get_yield func for new data finished

--- retrieve/get_new_data.py
+++ retrieve/get_new_data.py
@@ -1,10 +1,7 @@
 import pandas as pd
-from pathlib import Path
+EXCEL_PATH = "./new_excel_data/newYieldData.xlsx"
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-EXCEL_PATH = BASE_DIR / "new_excel_data" / "newYieldData.xlsx"
-
-BRAZIL = "./new_excel_data/Brazil3.csv"
+BRAZIL = "./new_excel_data/Brazil{0}.csv"
 HK = "./new_excel_data/HK{0}.csv"
 INDIA = "./new_excel_data/India{0}.csv"
 JAPAN = "./new_excel_data/Japan{0}.csv"
@@ -16,21 +13,43 @@ US = "./new_excel_data/US{0}.csv"
 
 def get_yield(term: int):
     if term not in [1, 2, 3, 5]:
-        print('invalid term')
-        return
+        raise ValueError('invalid term')
 
     term_s = f'{term} year'
     cols = ['US', 'UK', 'FRA', 'GER', 'AUS', 'INDO', 'KOREA', 'BRAZIL', 'MEXICO']
+
+    data_map = {
+        'US': US,
+        'KOREA': KOREA,
+        'BRAZIL': BRAZIL,
+        'MEXICO': MEXICO,
+        'HK': HK,
+        'INDIA': INDIA,
+        'JAPAN': JAPAN,
+        'SWITZ': SWITZ,
+    }
+
+    replace_map = {
+        1: ['KOREA', 'MEXICO'],
+        2: ['US'],
+        3: ['BRAZIL'],
+        5: ['BRAZIL', 'MEXICO']
+    }
+
+    cols_to_replace = replace_map.get(term)
     cols_to_add = ['HK', 'INDIA', 'JAPAN', 'SWITZ']
+    cols_to_add.extend(cols_to_replace)
 
     df = pd.read_excel(EXCEL_PATH, sheet_name=term_s, index_col=0)
     df.index = pd.to_datetime(df.index, format="%Y-%m-%d %H:%M:%S", errors="coerce")
     df = df[df.index.notna()]
     df.columns = cols
 
-    extra_data = [HK, INDIA, JAPAN, SWITZ]
+    df = df.drop(columns=cols_to_replace)
+
     result = []
-    for data in extra_data:
+    for col in cols_to_add:
+        data = data_map.get(col)
         df_extra = pd.read_csv(data.format(term), index_col=0)
         df_extra = df_extra.iloc[:, [0]]
         df_extra.index = pd.to_datetime(df_extra.index, format="%m/%d/%Y", errors="coerce")
@@ -40,16 +59,5 @@ def get_yield(term: int):
     extra_df = pd.concat(result, axis=1)
     extra_df.columns = cols_to_add
     df = pd.concat([df, extra_df], axis=1)
-
-    # if term == 1:
-    #     df_mex = pd.read_csv(MEXICO.format(term), index_col=0)
-    #     df_kor = pd.read_csv(KOREA.format(term), index_col=0)
-    # elif term == 2:
-    
-    # elif term == 3:
         
     return df
-
-df = get_yield(1)
-print(df.head(), '\n')
-print(df.tail())
